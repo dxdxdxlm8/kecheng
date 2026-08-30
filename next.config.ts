a301196dcd57095062b2eff3d71ecedf6073983e
zhixian <zhixian@example.com>
2026-08-30 09:56:32 +0800

    feat: /adminlogin 路径映射到原首页

--- next.config.ts
+++ next.config.ts
@@ -10,6 +10,14 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  async rewrites() {
+    return [
+      {
+        source: '/adminlogin',
+        destination: '/',
+      },
+    ];
+  },
 };
 
 export default nextConfig;
